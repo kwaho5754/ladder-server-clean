6e2b468f13914aa35fc88a44237728515ed61ba3
kwaho5754 <kwanho20@gmail.com>
2025-05-22 11:06:57 +0900

    🔧 main.py: 대칭 제거 및 시작방향+줄수+홀짝 블럭 분석 구조로 전체 수정

--- main.py
+++ main.py
@@ -16,47 +16,29 @@ def convert(entry):
 def split_components(name):
     return name[0], name[1], name[2]
 
-def mirror_component(c, mode):
-    if mode == 'side':
-        return '우' if c == '좌' else '좌'
-    elif mode == 'oe':
-        return '짝' if c == '홀' else '홀'
-    return c
-
-def analyze_component_blocks(data, mode, apply_mirror=False):
+def analyze_by_element(data, target_index):
     weights = {3: 1.0, 4: 1.5, 5: 2.0, 6: 2.5, 7: 3.0}
     scores = defaultdict(float)
 
-    components = []
-    for entry in data:
-        full = convert(entry)
-        side, count, oe = split_components(full)
-        val = side if mode == 'side' else oe
-        if apply_mirror:
-            val = mirror_component(val, mode)
-        components.append(val)
+    components = [split_components(convert(entry)) for entry in data]
 
     for size in range(3, 8):
         if len(components) < size:
             continue
         recent_block = components[-size:]
-        pattern = '>'.join(recent_block)
+        recent_pattern = [elem[target_index] for elem in recent_block]
 
         for i in range(len(components) - size):
-            past_block = components[i:i+size]
-            if pattern == '>'.join(past_block):
+            past_block = components[i:i + size]
+            past_pattern = [elem[target_index] for elem in past_block]
+            if recent_pattern == past_pattern:
                 weight = weights[size]
                 if i > 0:
-                    val = components[i-1]
-                    if apply_mirror:
-                        val = mirror_component(val, mode)
+                    val = components[i - 1][target_index]
                     scores[val] += weight / 2
                 if i + size < len(components):
-                    val = components[i+size]
-                    if apply_mirror:
-                        val = mirror_component(val, mode)
+                    val = components[i + size][target_index]
                     scores[val] += weight / 2
-
     return scores
 
 def top_elements(score_dict):
@@ -77,10 +59,9 @@ def predict():
 
         result = {
             "예측회차": round_num,
-            "시작방향 원본": top_elements(analyze_component_blocks(data, 'side', apply_mirror=False)),
-            "시작방향 대칭": top_elements(analyze_component_blocks(data, 'side', apply_mirror=True)),
-            "홀짝 원본": top_elements(analyze_component_blocks(data, 'oe', apply_mirror=False)),
-            "홀짝 대칭": top_elements(analyze_component_blocks(data, 'oe', apply_mirror=True))
+            "시작방향": top_elements(analyze_by_element(data, 0)),
+            "줄수": top_elements(analyze_by_element(data, 1)),
+            "홀짝": top_elements(analyze_by_element(data, 2))
         }
 
         return jsonify(result)
